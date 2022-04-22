c1ac3e59eb217e35a9722e9aa5032d8c267c717a
Ant20ni02 <antoniogarcort@hotmail.com>
2022-04-22 06:57:47 -0500

    Third Commit (Ya funciona, solo falta caso de paréntesis y signo negativo)

---
+++ app.py
@@ -0,0 +1,144 @@
+#Marco Antonio Gardida Cortés A01423221
+#Silvio Emmanuel Prieto Caro A01423341
+#Miguel Jiménez Padilla A01423189
+
+
+def signo(line, i):
+    if(line[i] == '*' or line[i] == '+' or line[i] == '-'  or line[i] == '^' or line[i] == '/' ):
+        return True
+    else:
+        return False
+
+
+def CasoVariable(line, i):
+    variable = ''
+    while(i<len(line)):
+        if(line[i].isalpha() or line[i].isdigit() or line[i] == '_'):
+            variable += line[i]
+        else:
+            break
+        i+=1
+
+    print(variable + '  ----->  Variable')
+    return i-1
+
+
+def casoE(line,i, numero):
+    i+=1
+    numero += line[i]
+    real = False
+    if(i=='-'):
+        real = True
+        i+=1
+        numero += line[i]
+    elif(i=='+'):
+        i+=1
+        numero += line[i]
+
+    if(line[i].isdigit()):
+        pass
+    else:
+        print(numero + '  ----->  Error')
+        return -1
+
+    while(i<len(line)):
+        if(line[i].isdigit()):
+            pass
+        else:
+            break
+        i+=1
+        
+
+    return i-1
+
+
+def CasoNumero(line, i):
+    numero = ''
+    flotante = False
+    countpunto = 0
+    while(i<len(line)):
+        if(line[i].isdigit()):
+            numero += line[i]
+        elif(line[i] == '.'):
+            if(line[i+1].isdigit() and countpunto < 1):
+                numero += line[i]
+                countpunto += 1
+                flotante = True
+            else:
+                print(numero + '  ----->  Error')
+                return -1
+        elif(line[i] == 'E' or line[i] == 'e'):
+            return casoE(line,i, numero)
+        else:
+            break
+        i+=1
+    if(flotante):
+        print(numero + '  ----->  Real')
+    else:
+        print(numero + '  ----->  Entero')
+
+    return i-1
+
+
+
+
+def lexerAritmetico(archivo):
+    with open(archivo) as f:
+        line = f.readline()
+        while line:
+            casooperacion = False
+            casovariableasignacion = True
+            comentario = True
+            casosolosigno = False
+            casoparentesis = False
+            line = f.readline()
+            line = line.replace(" ","")
+            i=0
+            while(i<len(line)-1):
+                if(comentario and line[i] == '/' and line[i+1] == '/'):
+                    print(line[i:] + '  ----->  Comentario')
+                    break
+                elif(line[i].isalpha()):
+                    i = CasoVariable(line, i)
+                    if(casovariableasignacion):
+                        i+=1
+                        if(line[i] == '='):
+                            print(line[i] + '          ----->       Asignación')
+                            casovariableasignacion = False
+                            comentario = False
+                        else:
+                            print(line[i] + '          ----->       Error')
+                            break
+                    else:
+                        comentario = True
+                        casooperacion = False
+                elif(line[i].isdigit()):#Caso números
+                    i = CasoNumero(line,i)
+                    comentario = True
+                    casocperacion = False
+                    if(i == -1):
+                        break
+                elif(signo(line, i)):
+                    if(line[i] == '+'):
+                        print(line[i] + '  ----->  Suma')
+                    elif(line[i] == '*'):
+                        print(line[i] + '  ----->  Multiplicación')
+                    elif(line[i] == '-'):
+                        print(line[i] + '  ----->  Resta')
+                    elif(line[i] == '/' ):
+                        print(line[i] + '  ----->  División')
+                    elif(line[i] == '^'):
+                        print(line[i] + '  ----->  Potencia')
+                    casocperacion = True
+                else:
+                    print(line[i] + '          ----->       Error')
+
+                i+=1
+                print('Kiti: ' + str(i))
+                
+
+
+
+#file = input()
+#lexerAritmetico(file)
+lexerAritmetico('p1.txt')
